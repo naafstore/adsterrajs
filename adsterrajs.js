cfcdd44c1ffc0093ba3529131929422d3d8a9575
naafstore <86194924+naafstore@users.noreply.github.com>
2021-12-25 21:22:33 +0700

    Update adsterrajs.js

--- adsterrajs.js
+++ adsterrajs.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : '71ddab5b44df9271be98ae95da6aacaa',
+		'key' : 'f54489e3041489f2b22d7cb5a51852bd',
 		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
+		'height' : 90,
+		'width' : 728,
 		'params' : {}
 };
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://functionssubqueries.com/71ddab5b44df9271be98ae95da6aacaa/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://functionssubqueries.com/f54489e3041489f2b22d7cb5a51852bd/invoke.js"></scr' + 'ipt>');
