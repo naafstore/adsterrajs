debfbc90a3c7f240d65ea00c0b2dcaa4ba4cbc19
naafstore <86194924+naafstore@users.noreply.github.com>
2021-12-25 21:10:17 +0700

    Create adsterrajs.js

---
+++ adsterrajs.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '71ddab5b44df9271be98ae95da6aacaa',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://functionssubqueries.com/71ddab5b44df9271be98ae95da6aacaa/invoke.js"></scr' + 'ipt>');
