72c759c0255a87e66c2e9f4a3f513b47cfe78cc8
naafstore <86194924+naafstore@users.noreply.github.com>
2023-01-22 17:25:05 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '1817c8025e4a1f622fcbe07888a042e4',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://functionssubqueries.com/1817c8025e4a1f622fcbe07888a042e4/invoke.js"></scr' + 'ipt>');
