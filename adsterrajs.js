f65aa929a9678135c492c3c9a4db6990e9656619
naafstore <86194924+naafstore@users.noreply.github.com>
2023-01-22 16:49:44 +0700

    Update and rename adsterrajs.js to adsterrajs728x90.js

--- adsterrajs.js
+++
@@ -1,8 +0,0 @@
-atOptions = {
-		'key' : 'f54489e3041489f2b22d7cb5a51852bd',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-};
-document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://functionssubqueries.com/f54489e3041489f2b22d7cb5a51852bd/invoke.js"></scr' + 'ipt>');
---
+++ adsterrajs728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '0daf9c862bec56d023a8b4633cc7a781',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://functionssubqueries.com/0daf9c862bec56d023a8b4633cc7a781/invoke.js"></scr' + 'ipt>');
