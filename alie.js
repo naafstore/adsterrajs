ab5cdcbba4955d020ed9db1cac012b91a6f244d1
naafstore <86194924+naafstore@users.noreply.github.com>
2023-02-04 10:33:08 +0700

    Update alie.js

--- alie.js
+++ alie.js
@@ -1,1 +1,1 @@
-href='https://s.click.aliexpress.com/e/_Ddtm0t3?bz=300*250' target='_parent'><img width='725' height='90' src='https://ae01.alicdn.com/kf/S3619e57974f148d087c950fe497cdf55q/300x250.jpg'
+document.write('<a href=\'https://s.click.aliexpress.com/e/_Ddtm0t3?bz=300*250\' target=\'_parent\'><img width=\'725\' height=\'90\' src=\'https://ae01.alicdn.com/kf/S3619e57974f148d087c950fe497cdf55q/300x250.jpg\' /></a>');
