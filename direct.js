720d6520113c852ceba2fda37bbfbff54af8d80f
naafstore <86194924+naafstore@users.noreply.github.com>
2023-10-27 21:24:03 +0700

    Update direct.js

--- direct.js
+++ direct.js
@@ -1,1 +1,1 @@
-<script src='https://functionssubqueries.com/bhg31090b?key=3e7c5ff7e2f9fa87a8f2ba710088216c'/>
+<script src='https://functionssubqueries.com/bhg31090b?key=3e7c5ff7e2f9fa87a8f2ba710088216c'/></script>
